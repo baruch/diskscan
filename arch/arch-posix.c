a9e93553e457133e4aa01000498ceba94fec5337
Baruch Even <baruch@ev-en.org>
2015-05-22 17:10:50 +0300

    Handle io_result in posix arch dependent code

--- arch/arch-posix.c
+++ arch/arch-posix.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <memory.h>
 
 bool disk_dev_open(disk_dev_t *dev, const char *path)
 {
@@ -15,12 +16,56 @@ void disk_dev_close(disk_dev_t *dev)
 	dev->fd = -1;
 }
 
-ssize_t disk_dev_read(disk_dev_t *dev, uint64_t offset_bytes, uint32_t len_bytes, void *buf)
+ssize_t disk_dev_read(disk_dev_t *dev, uint64_t offset_bytes, uint32_t len_bytes, void *buf, io_result_t *io_res)
 {
-	return pread(dev->fd, buf, len_bytes, offset_bytes);
+	ssize_t ret = pread(dev->fd, buf, len_bytes, offset_bytes);
+	if (ret == len_bytes) {
+		io_res->data = DATA_FULL;
+		io_res->error = ERROR_NONE;
+		return ret;
+	} else if (ret > 0) {
+		io_res->data = DATA_PARTIAL;
+		io_res->error = ERROR_NONE;
+		return ret;
+	} else if (ret == 0) {
+		io_res->data = DATA_NONE;
+		io_res->error = ERROR_NONE;
+		return ret;
+	} else {
+		// ret < 0, i.e. error
+		io_res->data = DATA_NONE;
+		io_res->error = ERROR_UNCORRECTED;
+		io_res->sense_len = 0;
+		memset(&io_res->info, 0, sizeof(io_res->info));
+		return -1;
+	}
+
+	//TODO: Handle EINTR with a retry
 }
 
-ssize_t disk_dev_write(disk_dev_t *dev, uint64_t offset_bytes, uint32_t len_bytes, void *buf)
+ssize_t disk_dev_write(disk_dev_t *dev, uint64_t offset_bytes, uint32_t len_bytes, void *buf, io_result_t *io_res)
 {
-	return pwrite(dev->fd, buf, len_bytes, offset_bytes);
+	ssize_t ret = pwrite(dev->fd, buf, len_bytes, offset_bytes);
+	if (ret == len_bytes) {
+		io_res->data = DATA_FULL;
+		io_res->error = ERROR_NONE;
+		return ret;
+	} else if (ret > 0) {
+		io_res->data = DATA_PARTIAL;
+		io_res->error = ERROR_NONE;
+		return ret;
+	} else if (ret == 0) {
+		io_res->data = DATA_NONE;
+		io_res->error = ERROR_NONE;
+		return ret;
+	} else {
+		// ret < 0, i.e. error
+		io_res->data = DATA_NONE;
+		io_res->error = ERROR_UNCORRECTED;
+		io_res->sense_len = 0;
+		memset(&io_res->info, 0, sizeof(io_res->info));
+		return -1;
+	}
+
+	//TODO: Handle EINTR with a retry
 }
